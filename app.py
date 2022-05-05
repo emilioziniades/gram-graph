33c1271db49cb8ecf20616e7432db58afd2b1861
Emilio Ziniades <emilioziniades@protonmail.com>
2022-05-05 13:35:15 +0200

    flask shows plotly figure

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+import json
+
 import dotenv
-from flask import Flask
+from flask import Flask, render_template
+import plotly
+
+from graph import GramGraph
 
 dotenv.load_dotenv()
 
@@ -7,5 +12,15 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello world</h1>"
+def index():
+    d = {
+        "a": ["b", "c"],
+        "b": ["c"],
+        "c": [],
+        "d": ["a", "b", "c", "d"],
+    }
+    G = GramGraph(d)
+    figure = G.plot_graph()
+    figure_JSON = json.dumps(figure, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template("base.html", figure_JSON=figure_JSON)
