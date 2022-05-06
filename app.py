4b7e4fb30ea1a9a8d37099257f7ccb7ce7ccf26e
Emilio Ziniades <emilioziniades@protonmail.com>
2022-05-06 13:36:52 +0200

    improved flask app
    - ability to toggle between pruned and unpruned graphs
    - preloads pruned and unpruned graph JSON once
    - refactored getting graph JSON into function

--- app.py
+++ app.py
@@ -1,27 +1,39 @@
 import json
 import pickle
+from typing import Tuple
 
-import dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import plotly
 
 from graph import GramGraph, test_data
 
-dotenv.load_dotenv()
-
 app = Flask(__name__)
 
-with open("followers.pickle", "rb") as f:
-    followers = pickle.load(f)
 
-# G = GramGraph(test_data)
-G = GramGraph(followers)
-figure = G.plot_graph()
-figure_JSON = json.dumps(figure, cls=plotly.utils.PlotlyJSONEncoder)
+def get_figures_JSON() -> Tuple[str, str]:
+    """Preloads JSON for both pruned and unpruned graphs"""
+    with open("followers.pickle", "rb") as f:
+        followers = pickle.load(f)
+    G = GramGraph(followers, prune=False)
+    GP = GramGraph(followers, prune=True)
+    unpruned_fig = G.plot_graph()
+    pruned_fig = GP.plot_graph()
+    return (
+        json.dumps(pruned_fig, cls=plotly.utils.PlotlyJSONEncoder),
+        json.dumps(unpruned_fig, cls=plotly.utils.PlotlyJSONEncoder),
+    )
+
+
+pruned_JSON, unpruned_JSON = get_figures_JSON()
 
 
 @app.route("/")
 def index():
+    print(request.args)
+    prune = bool(request.args.get("prune", False))
     return render_template(
-        "base.html", figure_JSON=figure_JSON, main_account="happyhoundsza"
+        "base.html",
+        figure_JSON=pruned_JSON if prune else unpruned_JSON,
+        main_account="happyhoundsza",
+        prune=prune,
     )
